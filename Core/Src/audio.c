ceea45cded45320ea1167f7f1b7036480f39c6ea
Chris Paine <christopherjpaine@gmail.com>
2023-09-26 22:06:13 +0100

    Audio: Start SAI Receive in audio.c, replacing BSP_Record(). Also remove output side callbacks.

--- Core/Src/audio.c
+++ Core/Src/audio.c
@@ -51,22 +51,33 @@ void AUDIO_Start (SAI_HandleTypeDef* audio_in_sai, SAI_HandleTypeDef* audio_out_
 
     audio_Init();
 
+    /* TODO Set Callbacks */
+
     /* End of init - onto start */
 
-    // BSP_AUDIO_IN_OUT_Play(inputBufferLR, AUDIO_BUFFER_SAMPLES);
-    /* Call the audio Codec Play function - The interface says it needs buffer 
-     * And size, this is not the case. */
+    /* Unmute the Audio CODEC 
+     * Note that the API says it needs buffer and size, this is not the case
+     * so we send NULL and 0. */
     if (wm8994_drv.Play(AUDIO_I2C_ADDRESS, NULL, 0) != 0)
     {
         __BKPT();
     }
-    /* Update the Media layer and enable it for play */
+
+    /* Begin Transmitting on the Output SAI. This is used for debug purposes
+     * really, makes it easy to verify audio is being rx'd correctly. And
+     * gives you a usable audio output if you need it. */
     HAL_StatusTypeDef s = HAL_SAI_Transmit_DMA(audio_out_sai, inputBufferLR, AUDIO_BUFFER_SAMPLES);
     if (HAL_OK != s) {
         __BKPT();
     }
 
-    BSP_AUDIO_IN_Record(inputBufferLR, AUDIO_BUFFER_SAMPLES);
+    /* Start receiving on the Input SAI. We will then drive the audio task
+     * from the interrupts of this peripheral. */
+    s = HAL_SAI_Receive_DMA(audio_in_sai, inputBufferLR, AUDIO_BUFFER_SAMPLES);
+    if(HAL_OK != s) {
+        __BKPT();
+    }
+
 }
 
 void    BSP_AUDIO_IN_TransferComplete_CallBack(void) {
@@ -81,68 +92,11 @@ void    BSP_AUDIO_IN_HalfTransfer_CallBack(void){
 	count++;
 }
 
-void    BSP_AUDIO_OUT_TransferComplete_CallBack(void) {
-	count--;
-}
-void    BSP_AUDIO_OUT_HalfTransfer_CallBack(void){
-	count--;
-}
-
-#if 1
-
 static void audio_Init( void ) {
 
     uint32_t sampleRate = BSP_AUDIO_FREQUENCY_48K;
 
-    /* BSP_AUDIO_IN_InitEx() */
-
-    /* De-initialise both sai blocks. */
-    // SAIx_In_DeInit();   
-
-    /* Configure the PLL to generate the output clock. */
-    /* I beleive that this is already configured by our generated code. */
-    // this function doesn't actually use the handle. Just the frequency.
-    // BSP_AUDIO_OUT_ClockConfig(&haudio_in_sai, sampleRate, NULL);
-
-    /* Configure the output side pins. 
-     * Because we pass the in handle to this out function it does not actually
-     * set the DMA at all. */
-    // BSP_AUDIO_OUT_MspInit(&haudio_in_sai, NULL);
-
-    /* Configure the input side pins and dma. 
-     * Just calls directly to SAI_AUDIO_IN_MspInit(&haudio_in_sai, NULL); under the hood. 
-     * Which we will need to use if we have the handle stored in this scope. */
-    // probably all set up by our own configuration in the ioc
-    // BSP_AUDIO_IN_MspInit();
-
-    /* This sets up both the input and output blocks of the SAI such that 
-     * the output is master tx and the input is slave rx. */
-    // could be replaced by our own configuration?
-//    SAIx_In_Init(sampleRate);                                               // This is necessary for it to work?
-
-    /* Initialise the codec with full volume */
-    // audio_InitCodec(sampleRate, 100);
-
-    /* BSP_AUDIO_IN_OUT_Init() */
-
-    /* Disable SAI */
-//   SAIx_In_DeInit();                                                       // REPEAT
-
-    /* Initialise the PLL to generate the output clock */
-//    BSP_AUDIO_OUT_ClockConfig(&haudio_in_sai, sampleRate, NULL);             // REPEAT
-
-    /* Initialise the output pins and dma */
-    // BSP_AUDIO_OUT_MspInit(&haudio_out_sai, NULL);
-
-    /* As above in other function, call BSP_AUDIO_IN_MspInit but because it's
-     * the line in it just calls direct to sai init. */
-    // SAI_AUDIO_IN_MspInit(&haudio_in_sai, NULL);                             // REPEAT
-
-    /* As above - set up both the input and output blocks for I2S */
-    // SAIx_In_Init(sampleRate);                                                // REPEAT
-
-    /* Initialise the codec with full volume */
-    audio_InitCodec(sampleRate, 100);                                       // REPEAT
+    audio_InitCodec(sampleRate, 100);
 
 }
 
@@ -162,4 +116,3 @@ static void audio_InitCodec (uint32_t sampleRate, uint32_t volumePercent) {
 
 }
 
-#endif
